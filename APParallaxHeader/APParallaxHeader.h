df8d201e7dc339d32f227251f9277298577c5d6c
Mike Silvis <mikesilvis@gmail.com>
2016-11-18 13:41:09 -0800

    Initial Commit

---
+++ APParallaxHeader/APParallaxHeader.h
@@ -0,0 +1,19 @@
+//
+//  APParallaxHeader.h
+//  APParallaxHeader
+//
+//  Created by Mike Silvis on 11/18/16.
+//  Copyright © 2016 Mike Silvis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for APParallaxHeader.
+FOUNDATION_EXPORT double APParallaxHeaderVersionNumber;
+
+//! Project version string for APParallaxHeader.
+FOUNDATION_EXPORT const unsigned char APParallaxHeaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <APParallaxHeader/PublicHeader.h>
+
+
